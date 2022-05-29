fa68097e6407d8168739d176718d5ae5956a0ee6
ShinNakamura <kokoronopython@gmail.com>
2022-05-29 18:39:24 +0900

    レコードをHashMap化するのをやめて単純にインデックスで指定し条件を検査するようにした。

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,48 @@
 use std::io::{self};
-use std::collections::HashMap;
 use csv;
 
 type MyResult = Result<(), Box<dyn std::error::Error>>;
-type RecordMap = HashMap<String, String>;
 
 pub fn run() -> MyResult {
-    let cond_map = get_cond();
-    println!("{:#?}", cond_map);
+    let conds = get_cond();
     let input = io::stdin();
     let input = io::BufReader::new(input.lock());
     let mut rdr = csv::Reader::from_reader(input);
     let out = io::stdout();
     let mut wtr = csv::Writer::from_writer(out.lock());
-    // ヘッダーの順番を守る
     let header = rdr.headers()?.clone();
     wtr.write_record(&header)?;
-    for result in rdr.deserialize() {
-        let record_map: RecordMap = result?;
-        let mut out: Vec<String> = Vec::new();
-        for fld in header.iter() {
-            if let Some(val) = record_map.get(fld) {
-                out.push(val.to_string());
+    for result in rdr.records() {
+        let record = result?;
+        let mut is_match = false;
+        for (i, fld) in header.iter().enumerate() {
+            if is_match {
+                break; // or 検索なので、一個でも条件がマッチしたら成功
+            }
+            let val = &record[i];
+            for (cond_fld, cond_val) in conds.iter() {
+                if fld != cond_fld {
+                    continue;
+                }
+                if val != cond_val {
+                    continue;
+                }
+                is_match = true;
             }
         }
-        wtr.write_record(&out);
+        if !is_match {
+            continue;
+        }
+        wtr.write_record(&record)?;
     }
     Ok(())
 }
 
 fn get_cond() -> Vec<(String, String)> {
-    let mut cond_map: Vec<(String, String)> = Vec::new();
+    let mut conds: Vec<(String, String)> = Vec::new();
     let args = std::env::args();
     if args.len() < 2 {
-        return cond_map;
+        return conds;
     }
     let mut is_cmd_name = true;
     for arg in args {
@@ -43,10 +52,10 @@ fn get_cond() -> Vec<(String, String)> {
         }
         let s: Vec<&str> = arg.splitn(2, ',').collect();
         if s.len() == 1 {
-            cond_map.push((s[0].to_string(), "".to_string()));
+            conds.push((s[0].to_string(), "".to_string()));
         } else {
-            cond_map.push((s[0].to_string(), s[1].to_string()));
+            conds.push((s[0].to_string(), s[1].to_string()));
         }
     }
-    cond_map
+    conds
 }
